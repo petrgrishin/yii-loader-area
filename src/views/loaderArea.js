ca86f69cdfed6306cffffe1d473d7defe21cef96
Petr Grishin <petr.grishin@grishini.ru>
2014-07-28 01:24:21 +0400

    Реализация загружаемой области

--- src/views/loaderArea.js
+++ src/views/loaderArea.js
@@ -2,9 +2,29 @@
  * Author: Petr Grishin <petr.grishin@grishini.ru>
  */
 App.register('{{id}}', function(params, scope, widgets) {
-
+    var $container = $('#' + params['containerId']);
+    var response = scope.createResponse();
+    var loader = scope.createLoader(response);
+    var load = function() {
+        loader.pull(params['url']);
+    };
+    response.bindLoad(function() {
+        $container.html(this.getContent());
+    });
+    response.bindApply(function() {
+        var context = this.getContext();
+        context && context.bindApply && context.bindApply(function() {
+            response.apply();
+            load();
+        });
+    });
+    load();
     return {
-        onApply: function() {
+        onApply: function(callback) {
+            response.bindApply(callback);
+        },
+        reload: function() {
+            load();
         }
     }
 });
